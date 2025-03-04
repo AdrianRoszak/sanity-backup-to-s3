b50e243ddfc38d2d52c2ea197c73af74697d234e
Weirdo <adroszak@weirdo.codes>
2025-03-04 20:56:56 +0100

    wip: extract scripts to separate files

---
+++ sanity_backup_to_s3/upload_to_s3.py
@@ -0,0 +1,17 @@
+import os
+import boto3
+
+
+def upload_to_s3(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except Exception as e:
+        print(f"Error uploading {file_name} to {bucket}: {e}")
+        return False
+    return True
